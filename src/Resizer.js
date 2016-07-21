dfb951e32759b461a7d0257fa2fd6f573e344d90
Bogdan Stefan <bogdan.stefan@hootsuite.com>
2016-07-21 13:24:08 +0300

    Add custom class names support

--- src/Resizer.js
+++ src/Resizer.js
@@ -10,9 +10,7 @@ export default React.createClass({
     },
 
     render() {
-        const split = this.props.split;
-        const classes = ['Resizer', split];
-        return <span className={classes.join(' ')} onMouseDown={this.onMouseDown} />
+        return <span className={this.props.className} onMouseDown={this.onMouseDown} />
     }
 });
 
